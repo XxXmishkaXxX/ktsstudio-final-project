de253273d361b8262ad1bb0a290555875127726f
Misha Kutsov <misha@misha.ru>
2025-09-26 20:19:42 +0500

    add store dir

---
+++ bot/app/store/__init__.py
@@ -0,0 +1,23 @@
+import typing
+
+from app.store.database.database import Database
+from app.store.cache.cache import Cache
+
+if typing.TYPE_CHECKING:
+    from app.web.app import Application
+
+
+class Store:
+    def __init__(self, app: "Application"):
+        pass
+
+def setup_store(app: "Application"):
+    app.database = Database(app)
+    app.on_startup.append(app.database.connect)
+    app.on_cleanup.append(app.database.disconnect)
+    
+    app.cache = Cache(app)
+    app.on_startup.append(app.cache.connect)
+    app.on_cleanup.append(app.cache.disconnect)
+    
+    app.store = Store(app)
