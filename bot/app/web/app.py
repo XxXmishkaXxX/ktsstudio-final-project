60576ace12c85596415656aae136c2a54ca8fb5a
Misha Kutsov <misha@misha.ru>
2025-09-26 20:18:21 +0500

    add setup app

---
+++ bot/app/web/app.py
@@ -0,0 +1,39 @@
+from aiohttp.web import (
+    Application as AiohttpApplication,
+    Request as AiohttpRequest,
+)
+from app.bot.client import TelegramBot, setup_telegram_client
+from app.rmq.rabbitmq import RabbitMQ, setup_rabbitmq
+from app.store.database.database import Database
+from app.store.cache.cache import Cache
+from app.store import Store, setup_store
+from app.web.config import Config, setup_config
+from app.web.logger import setup_logging
+from app.web.routes import setup_routes
+
+
+class Application(AiohttpApplication):
+    config: Config
+    database: Database
+    cache: Cache
+    store: Store
+    telegram: TelegramBot
+    rmq: RabbitMQ
+
+
+class Request(AiohttpRequest):
+    @property
+    def app(self) -> Application:
+        return super().app()
+
+
+def setup_app(config_path: str) -> Application:
+    app = Application()
+    setup_logging(app)
+    setup_config(app, config_path)
+    setup_store(app)
+    setup_rabbitmq(app)
+    setup_telegram_client(app)
+    setup_routes(app)
+
+    return app
