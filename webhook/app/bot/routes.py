0fbf199f16add4482374150bb0039ddeae061df5
Misha Kutsov <misha@misha.ru>
2025-09-26 14:54:25 +0500

    add webhook route

---
+++ webhook/app/bot/routes.py
@@ -0,0 +1,11 @@
+from aiohttp import web
+
+
+routes = web.RouteTableDef()
+
+@routes.post("/webhook")
+async def webhook(request: web.Request):
+    data = await request.json()
+    app = request.app
+
+    return web.Response(status=200)
