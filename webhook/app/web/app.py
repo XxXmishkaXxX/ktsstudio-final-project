fc2b1e2f9773235b017f12639e40f7b02483950c
Misha Kutsov <misha@misha.ru>
2025-09-26 14:55:42 +0500

    add application setup

---
+++ webhook/app/web/app.py
@@ -0,0 +1,34 @@
+from aiohttp.web import (
+    Application as AiohttpApplication,
+    Request as AiohttpRequest)
+
+from app.web.logger import setup_logging
+from app.web.config import Config, setup_config
+from app.web.routes import setup_routes
+from app.bot.webhook import TelegramBot, setup_webhook
+from app.rmq.rabbitmq import RabbitMQ, setup_rabbitmq
+
+class Application(AiohttpApplication):
+    config: Config
+    bot: TelegramBot
+    rmq: RabbitMQ
+
+class Request(AiohttpRequest):
+
+    @property
+    def app(self) -> Application:
+        return super().app()
+
+
+app = Application()
+
+
+async def setup_app(config_path: str) -> Application:
+    setup_logging(app)
+    setup_config(app, config_path)
+    setup_webhook(app)
+    setup_rabbitmq(app)
+    setup_routes(app)
+
+    return app
+    
