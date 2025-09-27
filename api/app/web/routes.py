0add21b6c7de6d4150ea6ca639e7d540a1eee9ad
Misha Kutsov <misha@misha.ru>
2025-09-27 10:57:03 +0500

    setup admin routes

--- api/app/web/routes.py
+++ api/app/web/routes.py
@@ -7,6 +7,8 @@ if TYPE_CHECKING:
 
 
 def setup_routes(app: "Application"):
+    from app.admin.routes import setup_routes as setup_admin_routes
+    setup_admin_routes(app)
     app.router.add_get(
         "/health", lambda request: web.json_response({"status": "ok"})
     )
