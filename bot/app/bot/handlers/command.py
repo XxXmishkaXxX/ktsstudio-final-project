22de4e2c5f5bcfb743934747bd6c0e98fd1a49b0
Misha Kutsov <misha@misha.ru>
2025-09-30 23:36:56 +0500

    add create_or_get  user in handle command.

--- bot/app/bot/handlers/command.py
+++ bot/app/bot/handlers/command.py
@@ -2,6 +2,7 @@ import typing
 
 from app.bot.handlers.commands.play import play_command
 from app.bot.handlers.commands.start import start_command
+from app.bot.handlers.utils.users import create_or_get_user
 
 if typing.TYPE_CHECKING:
     from app.web.app import Application
@@ -14,7 +15,9 @@ async def handle_command(
     user_data: dict,
     chat_type: str,
 ):
+    user = await create_or_get_user(app, user_data)
+
     if command == "/start":
-        await start_command(app, chat_id, user_data, chat_type)
+        await start_command(app, chat_id, user, chat_type)
     elif command == "/play":
-        await play_command(app, chat_id, user_data, chat_type)
+        await play_command(app, chat_id, user, chat_type)
