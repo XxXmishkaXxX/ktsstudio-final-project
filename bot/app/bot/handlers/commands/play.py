f463ae688a9b61fb5113fb9aca4cc7e3f0095d2f
Misha Kutsov <misha@misha.ru>
2025-09-30 23:37:23 +0500

    ref play command

--- bot/app/bot/handlers/commands/play.py
+++ bot/app/bot/handlers/commands/play.py
@@ -1,10 +1,7 @@
-import json
 import typing
 
-from app.bot.keyboards import join_game
-from app.bot.texts import get_game_text
 from app.games.service import GameService
-from app.users.models import State
+from app.users.models import State, User
 
 if typing.TYPE_CHECKING:
     from app.web.app import Application
@@ -13,11 +10,9 @@ if typing.TYPE_CHECKING:
 async def play_command(
     app: "Application",
     chat_id: int,
-    user_data: dict,
+    user: User,
     chat_type: str,
 ):
-    tg_id = user_data["id"]
-
     if chat_type == "private":
         await app.telegram.send_message(
             chat_id,
@@ -28,28 +23,15 @@ async def play_command(
         )
         return
 
-    state = await app.store.users.get_state_user(tg_id)
-    if not state:
-        user = await app.store.users.create_user(tg_id, user_data["username"])
-        state = user.state
-
     is_game_in = await app.store.games.is_active_game_in_chat(chat_id)
 
-    if state == State.idle and not is_game_in:
+    if user.state == State.idle and not is_game_in:
         await app.telegram.send_message(
             chat_id,
-            f"🎮 Пользователь @{user_data.get('username', user_data['id'])} "
-            "запустил игру!",
+            f"🎮 Пользователь @{user.username} запустил игру!",
         )
-        game = GameService(app)
-        created_game = await game.create_game(chat_id)
-        text = get_game_text([], [], created_game.state)
-        await app.telegram.send_message(
-            chat_id,
-            text,
-            reply_markup=json.dumps(join_game(created_game.id)),
-            parse_mode="HTML",
-        )
+        service: GameService = GameService(app)
+        await service.create_game(chat_id)
         return
 
     await app.telegram.send_message(chat_id, "В чате уже идет игра, подождите.")
