26238c9e4575bf7b51d8613ae4bc68731904c99a
Misha Kutsov <misha@misha.ru>
2025-09-30 23:40:18 +0500

    add processing calbacks

--- bot/app/rmq/rmq_callback.py
+++ bot/app/rmq/rmq_callback.py
@@ -2,6 +2,7 @@ import json
 import typing
 
 import aio_pika
+from app.bot.handlers.callback import handle_callback
 from app.bot.handlers.command import handle_command
 
 if typing.TYPE_CHECKING:
@@ -13,7 +14,29 @@ async def rmq_callback(message: aio_pika.IncomingMessage, app: "Application"):
         try:
             payload = json.loads(message.body.decode())
 
-            if "message" in payload:
+            if "callback_query" in payload:
+                chat_id = payload["callback_query"]["message"]["chat"]["id"]
+
+                data = json.loads(payload["callback_query"]["data"])
+                callback_type = data["t"]
+                game_id = int(data["g"])
+                team = data.get("team")
+
+                message_id = payload["callback_query"]["message"]["message_id"]
+                callback_id = payload["callback_query"]["id"]
+                user_data = payload["callback_query"]["from"]
+
+                await handle_callback(
+                    app,
+                    callback_type,
+                    game_id,
+                    chat_id,
+                    callback_id,
+                    message_id,
+                    user_data,
+                    team=team,
+                )
+            elif "message" in payload:
                 msg = payload["message"]
                 chat_id = msg["chat"]["id"]
                 text = msg.get("text", "")
