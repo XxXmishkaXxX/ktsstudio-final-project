a9b9cbecbc30b3893dd460e31cf4f17ced62ddde
Misha Kutsov <misha@misha.ru>
2025-09-30 18:27:27 +0500

    ref user accessor

--- bot/app/store/users/accessor.py
+++ bot/app/store/users/accessor.py
@@ -1,35 +1,35 @@
 from app.store.base.accessor import BaseAccessor
-from app.users.models import State, User, UserState
+from app.users.models import State, User
 from sqlalchemy import select
 
 
 class UserAccessor(BaseAccessor):
     async def create_user(self, tg_id: int, username: str):
         async with self.app.database.session() as session:
-            user = User(tg_id=tg_id, username=username)
+            user = User(id=tg_id, username=username)
             session.add(user)
             await session.commit()
+            return user
 
     async def get_user(self, tg_id: int):
         async with self.app.database.session() as session:
             return (
-                await session.execute(select(User).where(User.tg_id == tg_id))
+                await session.execute(select(User).where(User.id == tg_id))
             ).scalar_one_or_none()
 
     async def set_state_user(self, tg_id: int, state: str):
         async with self.app.database.session() as session:
-            user = (
-                await session.execute(select(User).where(User.tg_id == tg_id))
-            ).scalar_one_or_none()
+            user = await self.get_user(tg_id)
             if not user:
-                return None
+                return
 
-            user_state = user.state
-            if user_state:
-                user_state.state = State(state)
-            else:
-                user_state = UserState(user_id=user.id, state=State(state))
-                session.add(user_state)
-
+            user.state = State(state)
             await session.commit()
-            return user_state
+
+    async def get_state_user(self, tg_id: int):
+        async with self.app.database.session() as session:
+            return (
+                await session.execute(
+                    select(User.state).where(User.id == tg_id)
+                )
+            ).scalar_one_or_none()
