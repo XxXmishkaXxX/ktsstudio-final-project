9c8e39e65f09d3d79f50b1b64b23633e9369afc0
Misha Kutsov <misha@misha.ru>
2025-10-07 13:08:54 +0500

    ref stop_game cmd

--- bot/app/bot/handlers/commands/stop_game.py
+++ bot/app/bot/handlers/commands/stop_game.py
@@ -1,30 +1,33 @@
 import typing
 
-from app.games.service import GameService
 from app.users.models import User
 
 if typing.TYPE_CHECKING:
     from app.web.app import Application
 
-async def stop_game_command(app: "Application", chat_id: int, user: User, chat_type: str):
 
-    service: GameService = GameService(app)
+async def stop_game_command(
+    app: "Application", chat_id: int, user: User, chat_type: str
+):
 
     if chat_type == "private":
-        await app.telegram.send_message(chat_id, "Эта команда доступна только в чате с игроками")
+        await app.telegram.send_message(
+            chat_id, "Эта команда доступна только в чате с игроками"
+        )
         return
-    
+
     game = await app.store.games.is_active_game_in_chat(chat_id)
 
     if not game:
         await app.telegram.send_message(chat_id, "В чате нет активных игр")
         return
 
-    user_created_game = await service.get_user_created_game(game.id)
+    user_created_game = await app.game_service.get_user_created_game(game.id)
 
     if user.id != user_created_game:
-        await app.telegram.send_message(chat_id, "Эта команда доступна только, начавшему игру, пользователю")
+        await app.telegram.send_message(
+            chat_id, "Эта команда доступна только, начавшему игру, пользователю"
+        )
         return
-    
-    await service.stop_game(game.id, chat_id)
 
+    await app.game_service.stop_game(game.id, chat_id)
