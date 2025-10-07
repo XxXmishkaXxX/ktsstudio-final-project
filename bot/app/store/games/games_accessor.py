3798706149ebd348128250965770816c76f0837b
Misha Kutsov <misha@misha.ru>
2025-10-07 13:12:55 +0500

    ref accessors

--- bot/app/store/games/games_accessor.py
+++ bot/app/store/games/games_accessor.py
@@ -1,6 +1,6 @@
 from app.games.models.games import Game, GameState
 from app.store.base.accessor import BaseAccessor
-from sqlalchemy import select, update
+from sqlalchemy import select, update, and_
 
 
 class GameAccessor(BaseAccessor):
@@ -11,36 +11,41 @@ class GameAccessor(BaseAccessor):
             await session.commit()
             await session.refresh(game)
             return game
-        
+
     async def delete_game(self, game_id: int):
         async with self.app.database.session() as session:
             result = await session.execute(
-                select(Game).
-                where(
-                    Game.id == game_id)
+                select(Game).where(Game.id == game_id)
             )
             game = result.scalars().first()
             if game:
                 await session.delete(game)
                 await session.commit()
 
-
     async def is_active_game_in_chat(self, chat_id: int) -> Game | None:
         async with self.app.database.session() as session:
-            game = (
+            return (
                 await session.execute(
                     select(Game).where(
-                        Game.chat_id == chat_id
-                        and Game.state != GameState.finished
+                        and_(
+                            Game.chat_id == chat_id,
+                            Game.state != GameState.finished,
+                        )
                     )
                 )
             ).scalar_one_or_none()
-            return game
 
     async def get_game_by_id(self, game_id: int) -> Game | None:
         async with self.app.database.session() as session:
             result = await session.execute(
                 select(Game).where(Game.id == game_id)
+            )
+            return result.scalar_one_or_none()
+
+    async def get_game_by_chat_id(self, chat_id: int) -> Game | None:
+        async with self.app.database.session() as session:
+            result = await session.execute(
+                select(Game).where(Game.chat_id == chat_id)
             )
             return result.scalar_one_or_none()
 
@@ -58,3 +63,11 @@ class GameAccessor(BaseAccessor):
                 update(Game).where(Game.id == game_id).values(state=state.value)
             )
             await session.commit()
+
+    async def get_current_round_id(self, game_id: int):
+        async with self.app.database.session() as session:
+            return (
+                await session.execute(
+                    select(Game.current_round_id).where(Game.id == game_id)
+                )
+            ).scalar_one_or_none()
